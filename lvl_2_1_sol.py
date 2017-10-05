304023dea25fca61d66f0d2d4aecc995927281e5
Unknown <stephen.4d@gmail.com>
2017-10-04 23:08:44 -0500

    Removing wasteful conditional adding docuemntation

--- lvl_2_1_sol.py
+++ lvl_2_1_sol.py
@@ -1,9 +1,14 @@
 def answer(l):
     from itertools import permutations
     option = 0
-
+    #We reverse so we can start with the largest index number first instead of the usual inital index of 0.
+    #This is important since our goal is to find the largest number possible and quickly.
     for L in reversed(range(0, len(l) + 1)):
-        if len(str(option)) > L: break # this part isn't updating til the loop breaks
+        #Permutations will iterate through our inital array submitted (l) with whichever index value we're on (L)
+        #set is used to prevent us from having to iterate over duplicate permutations for arrays with duplicate values
+        #   IE [3,1,3] => permutations will spit out duplicate data when iterating through 3.
+        #       vs.
+        #      [1,2,3] => data will be different each iteration since no array values are the same.
         for subset in set(permutations(l, L)):
             sub = list(reversed(subset))
             if reduce(lambda x, y: (x * 10 + y) % 3, sub) == 0:
